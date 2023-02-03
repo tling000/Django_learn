6f6540482e6e662b678e2ec443f5f5d43903bdbc
tling <tling21490@gmail.com>
2023-02-03 23:53:34 +0900

    initial commit

---
+++ blogproject/blogproject/settings_local_sample.py
@@ -0,0 +1,15 @@
+from pathlib import Path
+
+BASE_DIR = Path(__file__).resolve().parent.parent
+
+# SECURITY WARNING: keep the secret key used in production secret!
+SECRET_KEY = ""
+
+# Database
+# https://docs.djangoproject.com/en/4.1/ref/settings/#databases
+DATABASES = {
+    "default": {
+        "ENGINE": "",
+        "NAME": "",
+    }
+}
