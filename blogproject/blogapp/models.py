ed0209a8f49abed2a90a6b41fd1fce8616090a04
tling <tling21490@gmail.com>
2023-02-05 00:51:49 +0900

    UPDATE: Create "Contents" model

--- blogproject/blogapp/models.py
+++ blogproject/blogapp/models.py
@@ -1,3 +1,8 @@
 from django.db import models
 
 # Create your models here.
+class Contents(models.Model):
+  title = models.CharField(max_length=100)
+  image = models.ImageField(upload_to="media/")
+  body = models.TextField()
+  published_at = models.DateTimeField(auto_now=True)
