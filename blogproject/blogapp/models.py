2adbc2232efc0f9a40c248d25941f8d32130ef03
tling <tling21490@gmail.com>
2023-02-05 02:10:30 +0900

    FIX: Modified model name to singular, UPDATE: Modified to display content titles in the admin screen

--- blogproject/blogapp/models.py
+++ blogproject/blogapp/models.py
@@ -1,8 +1,11 @@
 from django.db import models
 
 # Create your models here.
-class Contents(models.Model):
+class Content(models.Model):
   title = models.CharField(max_length=100)
   image = models.ImageField(upload_to="media/")
   body = models.TextField()
   published_at = models.DateTimeField(auto_now=True)
+
+  def __str__(self):
+    return self.title
